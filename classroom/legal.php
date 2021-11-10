146dbd2786a7385ee831bcd93fb776f4505df722
Maxence Lavenu <maxence.lavenu@outlook.fr>
2021-11-10 19:30:07 +0100

    Privacy policy, legal mentions and CGU

--- classroom/legal.php
+++ classroom/legal.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Classroom CGU</title>
+    <title>Classroom Legal Notices</title>
     <script src="assets/js/utils/listener.js"></script>
     <script src="/path.js"></script>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -35,7 +35,125 @@
 
 <body>
     <div id="legal-div">
-        <p>Place your Legal Notices here</p>
+        <h2>
+            Identification
+        </h2>
+        <p>
+            ARÉN Éducation
+            <br />
+            34 rue La Pérouse, Paris 75116 France
+        </p>
+        <p>
+            Société par Actions Simplifiée Capital social : 1 000 euros
+            <br />
+            Directeur général : Mélanie Rizk
+        </p>
+        <p>
+            <a href="https://areneducation.com/mentions-legales.html" title="Nous écrire">
+                info@areneducation.com
+            </a>
+        </p>
+        <p>
+            #RCS : 850 403 692 00013
+            <br />
+            # Identification fiscale TVA : FR23850403692
+        </p>
+        <div align="center">
+            <hr size="1" width="100%" noshade="" align="center" />
+        </div>
+        <h2>
+            Crédits
+        </h2>
+        <p>
+            Le graphisme et le développement de ce site ont été réalisés par la société
+            Option PME Inc.
+        </p>
+        <p>
+            Tous les éléments représentés dans ce site appartiennent à ARÉN Éducation,
+            sauf mention contraire. Tous droits de représentation, de reproduction et
+            d'adaptation réservés.
+        </p>
+        <p>
+            © ARÉN Éducation 2019.
+        </p>
+        <p>
+            Nous reconnaissons l'appui de Patrimoine Canada.
+        </p>
+
+        <h2>
+            Droits et devoirs des utilisateurs
+        </h2>
+        <p>
+            Les informations diffusées sur le site sont la propriété de ARÉN Éducation.
+        </p>
+        <p>
+            L'accès au site areneducation.com vous confère un droit d'usage privé et
+            non exclusif de ce site. L'ensemble des éléments édités sur ce site,
+            incluant notamment les textes, photographies, infographies, logos, marques…
+            constituent des œuvres au sens du code de la Propriété Intellectuelle. En
+            conséquence, toute représentation ou reproduction, intégrale ou partielle,
+            qui pourrait être faite sans le consentement de leurs auteurs ou de leurs
+            ayants-droit, est illicite.
+        </p>
+        <p>
+            Le non-respect de cette interdiction constitue une contrefaçon pouvant
+            engager la responsabilité civile et pénale du contrefacteur.
+        </p>
+        <h2>
+            Responsabilité
+        </h2>
+        <p>
+            ARÉN Éducation ne donne aucune garantie, explicite ou implicite, et
+            n'assume aucune responsabilité relative à l'utilisation de ces
+            informations.
+        </p>
+        <p>
+            ARÉN Éducation n'est pas responsable ni de l'exactitude, ni des erreurs, ni
+            des omissions contenues sur ce site. L'utilisateur est seul responsable de
+            l'utilisation de telles informations.
+        </p>
+        <p>
+            ARÉN Éducation se réserve le droit de modifier à tout moment les présentes
+            notamment en actualisant ce site.
+        </p>
+        <p>
+            ARÉN Éducation ne pourra être responsable pour quel que dommage que ce soit
+            tant direct qu'indirect, résultant d’une information contenue sur ce site.
+        </p>
+        <h2>
+            Indisponibilité du site
+        </h2>
+        <p>
+            ARÉN Éducation s'engage à faire ses meilleurs efforts pour assurer aux
+            utilisateurs une accessibilité du site à tout moment. ARÉN Éducation ne
+            pourra être tenu responsable, en cas d'indisponibilité du site, pour
+            quelque cause que ce soit.
+        </p>
+        <h2>
+            Droit applicable
+        </h2>
+        <p>
+            Les présentes Mentions légales sont soumises au droit interne français. La
+            langue des Mentions légales est la langue française. En cas de litige, les
+            tribunaux français seront seuls compétents.
+        </p>
+        <h2>
+            Hébergeur
+        </h2>
+        <p>
+            Centre de données OVH / Hébergement Web Canada
+            <br />
+            1030 Beaubien Est #103, Montréal, Québec H2S 1T4 Canada
+        </p>
+        <p>
+            Téléphone :
+            <a href="https://areneducation.com/mentions-legales.html" title="Téléphone">
+                +1.514.504.2113
+            </a>
+            <br />
+            Site Internet : <a href="https://whc.ca/" target="_blank" title="WHC">https://whc.ca/</a>
+        </p>
+
     </div>
     <script src="assets/js/lib/popper-1.12.9.min.js"></script>
     <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
--- classroom/privacy.php
+++ classroom/privacy.php
@@ -13,20 +13,15 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap"
-        rel="stylesheet" media='all'>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" media='all'>
     <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet" media='all'>
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-        rel="stylesheet" media='all'>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" media='all'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css"
-        integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
     <!-- Material icon -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
     <link rel="stylesheet" href="assets/css/theme.css">
     <link rel="stylesheet" href="assets/css/login.css">
@@ -40,7 +35,119 @@
 
 <body>
     <div id="cgv-div">
-        <p>Place your Privacy Policy here</p> 
+
+        <h1>Politique de protection des données personnelles</h1>
+        <p>
+            Dans le cadre de votre utilisation de l’Application « ARÉN+ » (ci-après, «
+            l’Application »), ARÉN Éducation collecte et traite des données à caractère
+            personnel. Toute information se rapportant à une personne physique
+            susceptible d’être identifiée directement ou indirectement (par exemple
+            nom, prénom, identifiant, adresse électronique, données de localisation
+            etc.) est une donnée personnelle (ci-après « Donnée(s) Personnelle(s) » ou
+            « Donnée(s) »).
+        </p>
+
+        <h2>Dans le cadre du GAR</h2>
+
+        <p>
+            Lorsque l’accès à l’Application se fait dans le cadre du Gestionnaire
+            d’Accès aux Ressources développé par le ministère de l’éducation nationale
+            (ci-après le « GAR »), les traitements de Données à caractère personnel
+            liés à l’accès et à l’utilisation de l’Application sont régis par les
+            textes qui encadrent le fonctionnement du GAR accessibles à l’adresse
+            suivante :
+            <a href="https://gar.education.fr/mentions-informatives-rgpd/">
+                https://gar.education.com/mentions-informatives-rgpd/
+            </a>
+        <p>
+            Dans ce cas, le ministère de l’éducation nationale est responsable du
+            traitement des Données.
+        </p>
+
+        <h2>
+            Utilisation
+        </h2>
+        <p>
+            ARÉN Éducation se préoccupe de la protection de la vie privée. Il n'y a pas
+            de donnée enregistrée qui vous reconnaît personnellement quand vous
+            naviguez sur le site. Toutefois, ARÉN Éducation utilise des
+            fichiers-témoins (« cookies ») pour trois fonctions :
+        </p>
+        <ul type="disc">
+            <li>
+                Gérer et faciliter l'accès aux fonctions de commande en ligne et
+                l'espace dédié aux membres;
+            </li>
+            <li>
+                Comptabiliser le nombre de visiteurs différents au cours d'une période
+                donnée (jour, semaine, mois, année). À ce sujet, le site utilise
+                <a href="https://policies.google.com/technologies/cookies?hl=fr" target="_blank" title="Google Analytics">
+                    Google Analytics
+                </a>
+                pour analyser l'audience et améliorer son contenu. Cliquer sur
+                <a href="https://policies.google.com/technologies/cookies?hl=fr" target="_blank" title="Google Analytics">
+                    Google Analytics
+                </a>
+                pour plus d'information sur les règles de confidentialité relatives à
+                son utilisation;
+            </li>
+            <li>
+                Analyser les processus de navigation sur le site afin d'en améliorer
+                l'ergonomie et d'en faciliter l'usage.
+            </li>
+        </ul>
+        <p>
+            Aucune utilisation commerciale n'est faite de toute donnée recueillie par
+            ARÉN Éducation.
+        </p>
+        <p>
+            Les renseignements que vous fournissez à ARÉN Éducation lorsque vous
+            complétez une commande, ne servent qu'à établir votre identité et à nous
+            permettre de prendre contact avec vous, au besoin. Ces renseignements nous
+            servent également pour les analyses statistiques de l'utilisation du site.
+            Ces renseignements ne sont utilisés à aucune autre fin et ne sont pas
+            fournis à des tiers. Vous pouvez vous désabonner en tout temps en
+            communiquant avec notre service à la clientèle.
+        </p>
+
+        <h2>
+            Hyperliens
+        </h2>
+        <p>
+            Lorsque vous cliquez sur un lien et que vous quittez notre espace Web, les
+            renseignements échangés à partir de ce moment-là ne sont plus assujettis à
+            notre politique de confidentialité, mais à celle du nouveau site visité.
+            Nous vous invitons, par mesure de prudence, à lire la politique de
+            confidentialité du site en question.
+        </p>
+
+        <h2>
+            « Cookies »
+        </h2>
+        <p>
+            Lorsque vous visitez notre site, de petits fichiers textes connus sous le
+            nom de « cookies » peuvent être sauvegardés sur le disque dur de votre
+            ordinateur. Nous personnalisons le contenu de certaines pages Web selon le
+            type de fureteur que vous utilisez ou toute autre information fournie par
+            nos « cookies ». Nous n'utilisons pas ces « cookies » pour rechercher des
+            informations personnelles dans votre ordinateur. Si vous rejetez les «
+            cookies », vous serez toujours capable de visiter notre site Web.
+        </p>
+        <p>
+            Les visiteurs du site sont par ailleurs informés que des « cookies »
+            enregistrent certaines informations qui sont stockées dans la mémoire de
+            leur disque dur. Ces informations servent à générer des statistiques
+            d’audience du site et à proposer des produits et services en fonction des
+            précédentes visites. Un message d’alerte peut demander au visiteur, au
+            préalable, s’il souhaite accepter les « cookies », ce qu’il peut bien
+            entendu refuser. Il est aussi possible de configurer son navigateur pour
+            accepter ou refuser les « cookies » du site areneducation.com à tout
+            moment.
+        </p>
+        <p>
+            Ces « cookies » ne contiennent pas d’informations confidentielles
+            concernant le visiteur.
+        </p>
     </div>
     <script src="assets/js/lib/popper-1.12.9.min.js"></script>
     <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
